62e9afdfa18dcf2a054000503c579354208ef009
Tatsuya Okayama (blendthink) <admin@blendthink.dev>
2024-03-02 23:26:11 +0900

    test: adjust expected output in tests

--- packages/cli_info_builder/test/cli_info_builder_test.dart
+++ packages/cli_info_builder/test/cli_info_builder_test.dart
@@ -37,20 +37,19 @@ dev_dependencies:
   build_runner: ^2.4.8
 ''';
 
-    const outputContent = '''
+    const outputContent = """
 // coverage:ignore-file
 // GENERATED CODE - DO NOT MODIFY BY HAND
 // ignore_for_file: type=lint
-// ignore_for_file: lines_longer_than_80_chars
 
 import 'package:cli_info/cli_info.dart';
 
 const cliInfo = CliInfo(
   name: 'eg',
-  description: 'An example CLI application using cli_info_builder.',
+  description: '''An example CLI application using cli_info_builder.''',
   version: '1.0.0-alpha',
 );
-''';
+""";
     await testBuilder(
       builder,
       {
